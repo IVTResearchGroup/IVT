598c907ffdfd230baaf694b3058284a1b1044bfd
IVTResearchGroup <vactresearchlab@gmail.com>
2024-09-26 17:01:10 +0800

    Add files via upload

---
+++ 192.168.1.16/lab/publications.js
@@ -0,0 +1,34 @@
+const publications = [
+    {
+        "title": "MeTACAST: Target- and Context-aware Spatial Selection in VR",
+        "authors": ["Lixiang Zhao", "Tobias Isenberg", "Fuqi Xie", "Hai-Ning Liang", "Lingyun Yu"],
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "year": 2024,
+        "image": "wp-content/uploads/2024/08/MeTACAST.jpg-1024x575.png",
+        "link": "https://yulingyun.com/publications/metacast/"
+    },
+    {
+        "title": "MeTACAST2: Target- and Context-aware Spatial Selection in VR",
+        "authors": ["Lixiang Zhao", "Tobias Isenberg", "Fuqi Xie", "Hai-Ning Liang", "Lingyun Yu"],
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "year": 2024,
+        "image": "wp-content/uploads/2024/08/cloudpoint_desktop-1-2048x717.png",
+        "link": "https://yulingyun.com/publications/metacast/"
+    },
+    {
+        "title": "MeTACAST3: Target- and Context-aware Spatial Selection in VR",
+        "authors": ["Tom", "Tobias Isenberg", "Fuqi Xie", "Hai-Ning Liang", "Lingyun Yu"],
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "year": 2024,
+        "image": "wp-content/uploads/2024/08/XJTLU-logo-raw.png",
+        "link": "https://yulingyun.com/publications/metacast/"
+    },
+    {
+        "title": "Another Research Paper",
+        "authors": ["Author 1", "Author 2"],
+        "journal": "ACM CHI",
+        "year": 2023,
+        "image": "wp-content/uploads/2024/08/AnotherResearch.jpg",
+        "link": "https://example.com/another-research-paper"
+    }
+];
